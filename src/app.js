d9b2ad4760db2e69354e97db8b95439dcbc9d73a
Muhammed Sheikh <mdsheikh9295@gmail.com>
2024-03-11 15:56:43 +0530

    Added registration routes, controllers, configured express in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.get('/test', (req,res)=>{
-    res.send('test');
-})
+// Express Configuration
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+// Express Configuration
 
-export {app};
+// Route Import
+ import userRouter from "./routes/user.route.js"
+// Route Import
+
+// Route declaration
+app.use("/api/v1/users", userRouter)
+// Route declaration
+
+export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {apiResponse} from "../utils/apiResponse.js";
+import {apiError} from "../utils/apiErrors.js";
+import {User} from "../models/user.model.js";
+
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+    const {username, email, password} = req.body;
+
+    if([username, email, password].some((field)=>field?.trim() === "")){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{username}, {email}]
+    });
+    if(existedUser){
+        throw new apiError(400, "Username or email already exists")
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    });
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if(!createdUser){
+        throw new apiError(500, "Something went wrong while registering a user")
+    }
+
+    return res
+        .status(201)
+        .json(new apiResponse(200, createdUser, "User registered successfully"));
+})
+
+
+export {
+    registerUser
+}
